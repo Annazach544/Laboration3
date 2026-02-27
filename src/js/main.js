2f67b7b03deb3f35efcc54a3105c39bea1dc4105
Anna Zach <anza2501@student.miun.se>
2026-02-27 12:08:43 +0100

    Färdig layout med SASS och startsida

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,1 @@
-import '../scss/style.scss';
-import javascriptLogo from '../assets/javascript.svg';
-const viteLogo = '/vite.svg'; // ligger i public
-import { setupCounter } from './counter.js';
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-  </div>
-`;
-
-setupCounter(document.querySelector('#counter'));
+import '../scss/main.scss';
